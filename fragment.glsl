c2f597965142d5810401e4740d55df2b6c561627
kuwoko <owavemail@gmail.com>
2020-09-01 18:29:45 +0200

    working atmospheric scattering

--- fragment.glsl
+++ fragment.glsl
@@ -3,24 +3,101 @@
 layout(location = 0) in vec4 v_position;
 layout(location = 0) out vec4 color;
 
-const float MIN_DIST = 0.01; const int MAX_DIST = 512;
-const int MAX_STEP = 256;
-const int ITER = 5;
-const float POWER = 5;
-const int FOV = 60;
+#define PI 3.14159265
 
+const float MIN_DIST = 0.01;
+const int MAX_DIST = 512;
+const int MAX_STEP = 256;
+
+const float radius_surface = 6360e3;
+const float radius_atmosphere = 6380e3;
+const vec3 rayleigh_coefficient = vec3(58e-7, 135e-7, 331e-7);
+const vec3 mie_coefficient = vec3(2e-5);
+const float sun_intensity = 20.;
+const vec3 earth_center = vec3(0.0, -radius_surface, 0.0);
+
+// parameters
 uniform int frames;
 uniform vec2 resolution;
 uniform vec3 box_size;
+uniform vec3 sun_direction = vec3(.5, .4 * (1. + 0.5), -1.);
 
-#define PI 3.141592
-#define iSteps 16
-#define jSteps 8
+vec2 density(vec3 point) {
+	float h = max(0.0, length(point - earth_center) - radius_surface);
+	return vec2(exp(-h / 8e3), exp(-h / 12e2));
+}
 
-uniform vec3 uSunPos = vec3(0, 0.5, -1.0);
+float escape(vec3 p, vec3 d, float R) {
+    vec3 v = p - earth_center;
+    float b = dot(v, d);
+    float det = b * b - dot(v, v) + R*R;
+    if (det < 0.) return -1.;
+    det = sqrt(det);
+    float t1 = -b - det, t2 = -b + det;
+    return (t1 >= 0.) ? t1 : t2;
+}
 
-vec3 skydome(vec3 dir) {
-	return vec3(1.0);
+// integrate densities for rayleigh and
+// mie scattering. (rayleigh, mie)
+vec2 scatter_depth(vec3 origin, vec3 direction, float l, float steps) {
+	// accumulate here
+	vec2 depth = vec2(0.0);
+
+
+	l /= steps;
+	depth *= l;
+
+	// iterate point
+	for (int i = 1; i < steps; ++i) {
+		depth += density(origin + direction * i);
+	}
+
+	return depth * l;
+}
+
+// some globals
+vec2 total_depth;
+vec3 intensity_rayleigh, intensity_mie;
+
+void scatter_in(vec3 origin, vec3 direction, float l, float steps) {
+	l /= steps;
+	direction *= l;
+
+	// iterate point
+	for (int i = 0; i < steps; ++i) {
+		vec3 p = origin + direction * i;
+		
+		// get density
+		vec2 d = density(p) * l;
+
+		// add
+		total_depth += d;
+
+		vec2 depth_sum = total_depth + scatter_depth(p, sun_direction, escape(p, sun_direction, radius_atmosphere), 4.0);
+
+		vec3 a = exp(-rayleigh_coefficient * depth_sum.x - mie_coefficient * depth_sum.y);
+
+		intensity_rayleigh += a * d.x;
+		intensity_mie += a * d.y;
+	}
+}
+
+vec3 skydome(vec3 origin, vec3 direction, float l, vec3 lo) {
+	total_depth = vec2(0.0);
+	intensity_rayleigh = vec3(0.0);
+	intensity_mie = vec3(0.0);
+	scatter_in(origin, direction, l, 16.0);
+	float mu = dot(direction, sun_direction);
+	// lo extinction
+	return 
+		lo * exp(-rayleigh_coefficient * total_depth.x - mie_coefficient * total_depth.y)
+		+
+		sun_intensity * (1.0 + mu * mu)
+		*
+		(intensity_rayleigh * rayleigh_coefficient * 0.0597 
+		 +
+		 intensity_mie * mie_coefficient * 0.196 / pow(1.58 - 1.52 * mu, 1.5)
+	);
 }
 
 // box signed distance function
@@ -50,21 +127,14 @@ float march(vec3 origin, vec3 dir) {
 }
 
 void main() {
-	vec3 light_direction = vec3(-0.5, -0.3, 0.2);
 	vec2 uv = gl_FragCoord.xy / resolution;
-	vec3 pos = vec3(0.0, 0.0, 10.0);
 	// calculate ray direction
 	vec2 xy = gl_FragCoord.xy - resolution.xy / 2.0;
-	float z = resolution.y / tan(radians(FOV) / 2.0);
-	vec3 dir = normalize(vec3(xy, -z));
-	float marched = march(pos, dir);
-	if (marched > 0.0) {
-		vec3 c = skydome(dir);
-		// Apply exposure.
-		c = 1.0 - exp(-1.0 * c);
-		color = vec4(c, 1.0);
-	} else {
-		color = vec4(vec3(1.0), 1.0);
-	}
+	float z = resolution.y / tan(radians(60) / 2.0);
+	vec3 O = vec3(0.0);
+	vec3 D = normalize(vec3(xy, -z));
+	vec3 col = vec3(0.0);
+	float L = escape(O, D, radius_atmosphere);
+	color = vec4(skydome(O, D, L, col), 0.0);
 }
 
